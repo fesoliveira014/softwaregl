a4a22b63cec330a1423701488fe785e67b8c96ec
Felipe Santos <felipe.santos@kryptus.com>
2017-03-15 16:52:43 -0300

    improved makefile and removed binary

--- SoftwareGL/main.cpp
+++ SoftwareGL/main.cpp
@@ -7,11 +7,13 @@ const TGAColor red = TGAColor(0, 0, 255, 255);
 
 int main(int argc, char * argv[])
 {
-    TGAImage image(100, 100, TGAImage::RGBA);
+    TGAImage image(100, 100, TGAImage::RGB);
 
     //img.ReadFile("floor.tga");
 
-    raster::line(10, 20, 70, 40, image, white);
+    raster::line(13, 20, 80, 40, image, white); 
+    raster::line(20, 13, 40, 80, image, red); 
+    raster::line(80, 40, 13, 20, image, red);
 
     image.WriteFile("output.tga", false);
 
