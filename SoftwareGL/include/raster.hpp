9f2b49cabe730ada27fc3c313bd4bda5f0887d62
Felipe Santos <felipe.santos@kryptus.com>
2017-03-23 17:48:54 -0300

    finished triangle rasterization algorithm

--- SoftwareGL/include/raster.hpp
+++ SoftwareGL/include/raster.hpp
@@ -7,6 +7,14 @@
 
 namespace raster
 {
+    struct bbox2d
+    {
+        glm::ivec2 min, max;
+
+        bbox2d() : min(glm::ivec2(0)), max(glm::ivec2(0)) {}
+        bbox2d(glm::ivec2 a, glm::ivec2 b) : min(a), max(b) {}
+    };
+
     static void line(const glm::ivec2 & aa, const glm::ivec2 & bb, TGAImage *image, TGAColor color)
     {
         glm::ivec2 a(aa);
@@ -42,14 +50,51 @@ namespace raster
         }
     }
 
+    static glm::vec3 barycentric(const glm::ivec2 &a, const glm::ivec2 &b, const glm::ivec2 &c, glm::ivec2 &p)
+    {
+        glm::vec2 ab = b - a;
+        glm::vec2 ca = c - a;
+        glm::vec2 pa = a - p;
+
+        glm::vec3 u = glm::cross(glm::vec3(ab.x, ca.x, pa.x), glm::vec3(ab.y, ca.y, pa.y));
+        
+        if (std::abs(u.z) < 1.f) 
+            return glm::vec3(-1,1,1);
+        
+        return glm::vec3(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x/u.z);
+    }
+
+    static void triangle(const glm::ivec2 &t1, const glm::ivec2 &t2, const glm::ivec2 &t3, const TGAColor &color, TGAImage *image)
+    {
+        glm::ivec2 points[3] = {glm::ivec2(t1), glm::ivec2(t3), glm::ivec2(t3)};
+        glm::ivec2 clamp(image->GetWidth() - 1, image->GetHeight() - 1);
+        bbox2d bbox(clamp, glm::ivec2(0));
+
+        for (int i = 0; i < 3; ++i) {
+            bbox.min = glm::max(glm::ivec2(0), glm::min(bbox.min, points[i]));
+            bbox.max = glm::min(clamp        , glm::max(bbox.min, points[i]));
+        }
+
+        glm::ivec2 p;
+
+        for (p.x = bbox.min.x; p.x <= bbox.max.x; ++p.x) {
+            for (p.y = bbox.min.y; p.y <= bbox.max.y; ++p.y) {
+                glm::vec3 bcScreen = barycentric(t1, t2, t3, p);
+                if (bcScreen.x >= 0 && bcScreen.y >= 0 && bcScreen.z >= 0) 
+                    image->Set(p.x, p.y, color);
+            }
+        }
+    }
+
     static void render(Model& model, TGAImage *image, bool wireframe = false)
     {
         TGAColor white = TGAColor(255, 255, 255, 255);
 
-        if (wireframe) {
-            for (uint i = 0; i < model.GetNumOfFaces(); ++i) {
-                Model::face face = model.GetFace(i);
-                
+        
+        for (uint i = 0; i < model.GetNumOfFaces(); ++i) {
+            Model::face face = model.GetFace(i);
+
+            if (wireframe) {        
                 for (uint j = 0; j < 3; ++j) {
                     glm::vec3 v0 = model.GetVertex(face[j].vertex);
                     glm::vec3 v1 = model.GetVertex(face[(j + 1) % 3].vertex);
@@ -61,6 +106,10 @@ namespace raster
 
                     line(a, b, image, white);
                 }
+            }
+
+            else {
+                
             }
         }
     }
--- SoftwareGL/src/main.cpp
+++ SoftwareGL/src/main.cpp
@@ -8,13 +8,22 @@ const TGAColor red = TGAColor(0, 0, 255, 255);
 
 int main(int argc, char * argv[])
 {
-    TGAImage image(1024, 1024, TGAImage::RGB);
-    Model model(std::string("../obj/diablo3_pose/diablo3_pose.obj"));
+    TGAImage frame(200, 200, TGAImage::RGB);
+    glm::ivec2 points[3] = {glm::ivec2(10,10), glm::ivec2(100, 30), glm::ivec2(190, 160)}; 
 
-    raster::render(model, &image, true);
+    raster::triangle(points[0], points[1], points[2], red, &frame);
+
+    frame.FlipVertically();
+    frame.WriteFile("triangle.tga", false);
+
+
+    // TGAImage image(1024, 1024, TGAImage::RGB);
+    // Model model(std::string("../obj/diablo3_pose/diablo3_pose.obj"));
+
+    // raster::render(model, &image, true);
     
-    image.FlipVertically();
-    image.WriteFile("output.tga", false);
+    // image.FlipVertically();
+    // image.WriteFile("output.tga", false);
 
     return 0;
 }
